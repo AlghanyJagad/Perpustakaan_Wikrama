aed9bfb99b50765262290f1a282ecb711e7726c8
AlghanyJagad <58999743+AlghanyJagad@users.noreply.github.com>
2021-03-02 09:21:40 +0700

    Fix tampilan login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,19 +1,25 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<title>Login Perpustakaan</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="{{asset('loginstyle/vendor/bootstrap/css/bootstrap.min.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('loginstyle/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('loginstyle/vendor/animate/animate.css')}}">	
-<link rel="stylesheet" type="text/css" href="{{asset('loginstyle/vendor/css-hamburgers/hamburgers.min.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('loginstyle/vendor/select2/select2.min.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('loginstyle/css/util.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('loginstyle/css/main.css')}}">
+    
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <link rel="stylesheet" type="text/css" href="{{asset('loginstyle/vendor/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('loginstyle/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('loginstyle/vendor/animate/animate.css')}}">	
+    <link rel="stylesheet" type="text/css" href="{{asset('loginstyle/vendor/css-hamburgers/hamburgers.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('loginstyle/vendor/select2/select2.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('loginstyle/css/util.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('loginstyle/css/main.css')}}">
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-	
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
@@ -23,6 +29,16 @@
 
 				<form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
                     @csrf
+
+                    @if(count($errors) > 0)
+                        @foreach( $errors->all() as $message )
+                        <div class="alert alert-danger display-hide">
+                            <button class="close" data-close="alert"></button>
+                            <span>{{ $message }}</span>
+                        </div>
+                        @endforeach
+                    @endif
+
 					<span class="login100-form-title">
 						{{ __('Login') }}
 					</span>
@@ -30,11 +46,11 @@
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
                         <input id="email" type="email" class="input100" @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        @if ($errors->has('email'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-envelope" aria-hidden="true"></i>
@@ -44,11 +60,11 @@
 					<div class="wrap-input100 validate-input" data-validate = "Password is required">
                         <input id="password" type="password" class="input100" @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                        @if ($errors->has('password'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-lock" aria-hidden="true"></i>
@@ -71,7 +87,7 @@
                                     </label>
                                 </div>
                             </div>
-                    </div>
+                        </div>
 
 					<div class="text-center p-t-12">
 						@if (Route::has('password.request'))
@@ -82,7 +98,7 @@
 					</div>
                     
 
-					<div class="text-center p-t-136">
+					<div class="text-center p-t-100">
 						
 					</div>
 				</form>
